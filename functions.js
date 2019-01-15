8598f90e44ad815f9e74ee691d30e284043911a1
BTurner15 <BTurner15@mail.greenriver.edu>
2019-01-14 19:47:07 -0800

    1st Commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+// Bruce Turner, IT 328, Winter 2019
+// Professor Tina Ostrander, Green River Community College
+// January 14 2019
+// file functions.js
+var person;
+// 
+// functions by alphabetical listing
+//		()
+//------------------------------------------------------------------------
+//								()
+//------------------------------------------------------------------------
+
+//------------------------------------------------------------------------
---
+++ js-review.js
@@ -0,0 +1,7 @@
+// Bruce Turner, IT 328, Winter 2018
+// Professor Tina Ostrander, Green River Community College
+// January 14 2019
+// file js-review.js
+// 
+var silent = true;  // for debugging
+console.log("Introduction to JavaScript");
