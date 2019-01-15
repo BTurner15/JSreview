3f1292de1fefe9e9626bf3cd4eeec3630a6dea9d
BTurner15 <BTurner15@mail.greenriver.edu>
2019-01-14 19:57:12 -0800

    2nd Commit

--- js-review.js
+++ js-review.js
@@ -5,3 +5,14 @@
 // 
 var silent = true;  // for debugging
 console.log("Introduction to JavaScript");
+// credit where credit is due
+//Twenty Ways to FizzBuzz: An exercise in Javascript
+//https://www.reddit.com/r/javascript/comments/3j2zgz/twenty_ways_to_fizzbuzz_an_exercise_in_javascript/
+//lachlanhunt
+
+for (var i = 1; i <= 100; i++) {
+    var n = (i % 3 === 0) ? "Fizz" : "";
+    n +=    (i % 5 === 0) ? "Buzz" : "";
+    console.log(n ? n : i); //OR console.log(n || i);
+}
+
